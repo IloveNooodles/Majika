fd5e751846d6f32b1d243090cc95b0a908492797
dxt99 <ericlouis19x@gmail.com>
2023-02-07 20:54:09 +0700

    feat: added searchbar ui on menu

---
+++ app/src/main/java/com/example/myapplication/ui/menu/MenuItem.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication.ui.menu
+
+class MenuItem(name: String, price: String, desc: String){
+    val name: String = name;
+    val price: String = price;
+    val desc: String = desc;
+}
