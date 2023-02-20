4d1699c7744142d1c447da4f4d2a9922a1791481
IloveNooodles <mgarebaldhie80@gmail.com>
2023-02-20 22:55:23 +0700

    feat: initialize new activity for keranjang and add fix button order

--- app/src/main/java/com/dba/majika/api/RetrofitClient.kt
+++ app/src/main/java/com/dba/majika/api/RetrofitClient.kt
@@ -4,15 +4,16 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val baseUrl = "https://eb53-2001-448a-3040-6667-390f-9749-8ef-6d90.ap.ngrok.io/v1/"
-
+    private const val baseUrl =
+        "https://011b-2001-448a-3040-6667-80ba-5512-259a-c8ca.ap.ngrok.io/v1/"
+    
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
-
+    
     val api: Api by lazy {
         retrofit.create(Api::class.java)
     }
