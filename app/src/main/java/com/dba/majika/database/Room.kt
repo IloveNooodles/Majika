8501b277c5c17b3b2b17cc723008b98027ba1809
IloveNooodles <mgarebaldhie80@gmail.com>
2023-02-21 17:30:10 +0700

    feat: keranjang dao

--- app/src/main/java/com/dba/majika/database/Room.kt
+++ app/src/main/java/com/dba/majika/database/Room.kt
@@ -4,16 +4,22 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.dba.majika.models.keranjang.KeranjangDao
+import com.dba.majika.models.keranjang.KeranjangDatabaseEntity
 import com.dba.majika.models.menu.MenuDao
 import com.dba.majika.models.menu.MenuDatabaseEntity
 import com.dba.majika.models.restaurant.RestaurantDatabaseEntity
 import com.dba.majika.models.restaurant.RestaurantsDao
 
 
-@Database(entities = [MenuDatabaseEntity::class, RestaurantDatabaseEntity::class], version = 1)
+@Database(
+    entities = [MenuDatabaseEntity::class, RestaurantDatabaseEntity::class, KeranjangDatabaseEntity::class],
+    version = 1
+)
 abstract class MajikaDatabase : RoomDatabase() {
     abstract val menuDao: MenuDao
     abstract val restaurantsDao: RestaurantsDao
+    abstract val keranjangDao: KeranjangDao
 }
 
 private lateinit var INSTANCE: MajikaDatabase
--- app/src/main/java/com/dba/majika/models/keranjang/KeranjangDao.kt
+++ app/src/main/java/com/dba/majika/models/keranjang/KeranjangDao.kt
@@ -1,4 +1,28 @@
 package com.dba.majika.models.keranjang
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
 interface KeranjangDao {
+    @Query("select * from keranjang")
+    fun getItems(): LiveData<List<KeranjangDatabaseEntity>>
+    
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertItem(keranjangItem: KeranjangItem)
+    
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(keranjangItems: List<KeranjangDatabaseEntity>)
+    
+    @Update
+    suspend fun addOneQuantity(id: Int)
+    
+    @Update
+    suspend fun removeOneQuantity(id: Int)
+    
+    @Delete
+    fun deleteItem(id: Int)
+    
+    @Query("delete from keranjang")
+    fun deleteAll()
 }
