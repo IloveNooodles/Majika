3a8ac8796fde175aad5045550654af42743c505a
IloveNooodles <mgarebaldhie80@gmail.com>
2023-02-21 13:39:21 +0700

    fix: plus minus sign in button not showing

--- app/src/main/java/com/dba/majika/PembayaranActivity.kt
+++ app/src/main/java/com/dba/majika/PembayaranActivity.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.dba.majika.databinding.ActivityPembayaranBinding
-import com.google.android.material.snackbar.Snackbar
 
 class PembayaranActivity : AppCompatActivity() {
     
@@ -20,21 +18,8 @@ class PembayaranActivity : AppCompatActivity() {
         binding = ActivityPembayaranBinding.inflate(layoutInflater)
         setContentView(binding.root)
         
-        setSupportActionBar(binding.toolbar)
-        
         val navController = findNavController(R.id.nav_host_fragment_content_keranjang)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-        
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
-    
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_keranjang)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
